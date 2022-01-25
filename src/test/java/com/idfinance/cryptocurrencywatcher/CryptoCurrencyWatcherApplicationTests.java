91100a1543934d55ab7b058f9467c34681dca2c1
Igor Strelchenya <just.sicarius@gmail.com>
2022-01-25 20:22:26 +0300

    Add initial files

---
+++ src/test/java/com/idfinance/cryptocurrencywatcher/CryptoCurrencyWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idfinance.cryptocurrencywatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoCurrencyWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
